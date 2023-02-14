9b07b7cf468195f68b450e2da2c9f1df15a17e03
shardhayv <shardhayvatshyayan7@gmail.com>
2023-02-14 22:01:45 +0545

    Network Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey[700],
       appBar: AppBar(
-        title: const Text('Hello'),
+        title: const Text('Shardhay'),
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1676318857285-1ba989cc6699?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+        ),
       ),
     ),
     debugShowCheckedModeBanner: false,
